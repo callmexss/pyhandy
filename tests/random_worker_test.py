98bf11c5e8486513d2fcb00f10abbefb1cf10a7e
callmexss <callmexss@126.com>
2019-06-19 19:56:45 +0800

    finish a function of RandomWorker

---
+++ tests/random_worker_test.py
@@ -0,0 +1,19 @@
+'''
+File:          random_worker_test.py
+File Created:  Wednesday, 19th June 2019 6:06:09 pm
+Author:        xss (callmexss@126.com)
+Description:   test random worker
+-----
+Last Modified: Wednesday, 19th June 2019 6:06:12 pm
+Modified By:   xss (callmexss@126.com)
+-----
+'''
+
+import sys
+import doctest
+import unittest
+
+class TestRandomWorker(unittest.TestCase):
+    def setup(self):
+        pass
+        # self.worker = RandomWorker()
